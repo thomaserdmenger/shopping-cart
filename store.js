2d7bd6a2927d067595e033f06b6d3d5f21b055a6
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-08-13 17:10:54 +0200

    Refactor set up store items

--- store.js
+++ store.js
@@ -1,32 +1,70 @@
 import items from "./items.json";
+import formatCurrency from "./utilities/formatCurrency.js";
 
-const heading2Arr = Array.from(document.querySelectorAll("h2"));
-const heading3Arr = Array.from(document.querySelectorAll("h3"));
-const priceArr = Array.from(document.querySelectorAll("p"));
-const image = Array.from(document.querySelectorAll("img"));
+const storeItemTemplate = document.querySelector("#store-item-template");
+const dataStoreContainer = document.querySelector("[data-store-container]");
+
+const IMG_SOURCE = "https://dummyimage.com/420x260";
+
+// src="https://dummyimage.com/420x260/F00/F00">
 
 export function setupStore() {
-  items.forEach((item, index) => {
-    // Render category
-    heading3Arr.forEach((heading, idx) =>
-      idx === index ? (heading.textContent = item.category) : null
-    );
-
-    // Render color
-    heading2Arr.forEach((heading, idx) =>
-      idx === index ? (heading.textContent = item.name) : null
-    );
-
-    // Render price
-    priceArr.forEach((price, idx) =>
-      idx === index
-        ? (price.textContent = `$${(item.priceCents / 100).toFixed(2)}`)
-        : null
-    );
-
-    // Image color
-    image.forEach((img, idx) =>
-      idx === index ? (img.style.backgroundColor = `#${item.imageColor}`) : null
-    );
-  });
+  // items.forEach(renderStoreItem); Same as:
+  items.forEach((item) => renderStoreItem(item));
 }
+
+function renderStoreItem(item) {
+  const storeItem = storeItemTemplate.content.cloneNode(true);
+
+  const container = storeItem.querySelector("[data-store-item]");
+  container.dataset.itemId = item.id;
+
+  const name = storeItem.querySelector("[data-name]");
+  name.textContent = item.name;
+
+  const category = storeItem.querySelector("[data-category]");
+  category.textContent = item.category;
+
+  const price = storeItem.querySelector("[data-price]");
+  price.textContent = formatCurrency(item.priceCents / 100);
+
+  const image = storeItem.querySelector("[data-image]");
+  image.setAttribute(
+    "src",
+    `${IMG_SOURCE}/${item.imageColor}/${item.imageColor}`
+  );
+
+  dataStoreContainer.append(container);
+}
+
+// OWN SOlUTION
+// const heading2Arr = Array.from(document.querySelectorAll("h2"));
+// const heading3Arr = Array.from(document.querySelectorAll("h3"));
+// const priceArr = Array.from(document.querySelectorAll("p"));
+// const image = Array.from(document.querySelectorAll("img"));
+
+// export function setupStore() {
+//   items.forEach((item, index) => {
+//     // Render category
+//     heading3Arr.forEach((heading, idx) =>
+//       idx === index ? (heading.textContent = item.category) : null
+//     );
+
+//     // Render color
+//     heading2Arr.forEach((heading, idx) =>
+//       idx === index ? (heading.textContent = item.name) : null
+//     );
+
+//     // Render price
+//     priceArr.forEach((price, idx) =>
+//       idx === index
+//         ? (price.textContent = `$${(item.priceCents / 100).toFixed(2)}`)
+//         : null
+//     );
+
+//     // Image color
+//     image.forEach((img, idx) =>
+//       idx === index ? (img.style.backgroundColor = `#${item.imageColor}`) : null
+//     );
+//   });
+// }
